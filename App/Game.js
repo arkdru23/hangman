cde6038de57f058e094365889578f8c9369422c5
arkdru23 <arkadiuszdrukala@MacBook-Pro.local>
2020-11-30 13:56:33 +0100

    add styles and letters to game

--- App/Game.js
+++ App/Game.js
@@ -1,5 +1,20 @@
 class Game {
-  constructor() {}
+  constructor({ lettersWrapper, categoryWrapper, wordWrapper, outputWrapper }) {
+    this.lettersWrapper = lettersWrapper;
+    this.categoryWrapper = categoryWrapper;
+    this.wordWrapper = wordWrapper;
+    this.outputWrapper = outputWrapper;
+  }
+  guess(letter) {
+    console.log(letter);
+  }
+  start() {
+    for (let i = 0; i < 26; i++) {
+      const label = (i + 10).toString(36);
+      const button = document.createElement("button");
+      button.innerHTML = label;
+      button.addEventListener("click", () => this.guess(label));
+      this.lettersWrapper.appendChild(button);
+    }
+  }
 }
-
-const game = new Game();
