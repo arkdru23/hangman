52b1d448765c22a1700410ccd530966903a8f535
arkdru23 <arkadiuszdrukala@MacBook-Pro.local>
2020-12-01 09:39:40 +0100

    change method start and create new method to draw letters

--- App/Game.js
+++ App/Game.js
@@ -1,14 +1,43 @@
+import { Quote } from "./Quote";
+
 class Game {
+  quotes = [
+    {
+      text: "pan tadeusz",
+      category: "Utwór Literacki",
+    },
+    {
+      text: "janko muzykant",
+      category: "Utwór Literacki",
+    },
+    {
+      text: "akademia pana kleksa",
+      category: "Film",
+    },
+    {
+      text: "ogniem i mieczem",
+      category: "Utwór Literacki",
+    },
+  ];
+
   constructor({ lettersWrapper, categoryWrapper, wordWrapper, outputWrapper }) {
     this.lettersWrapper = lettersWrapper;
     this.categoryWrapper = categoryWrapper;
     this.wordWrapper = wordWrapper;
     this.outputWrapper = outputWrapper;
+
+    const { text, category } = this.quotes[
+      Math.floor(Math.random() * this.quotes.length)
+    ];
+
+    this.Quote = new Quote(text);
   }
+
   guess(letter) {
     console.log(letter);
   }
-  start() {
+
+  drawLetters() {
     for (let i = 0; i < 26; i++) {
       const label = (i + 10).toString(36);
       const button = document.createElement("button");
@@ -16,5 +45,9 @@ class Game {
       button.addEventListener("click", () => this.guess(label));
       this.lettersWrapper.appendChild(button);
     }
+  }
+
+  start() {
+    this.drawLetters();
   }
 }
