5aea52491dffd9a38ac5b4321b6a786035cf9331
arkdru23 <arkadiuszdrukala@MacBook-Pro.local>
2020-12-01 10:57:25 +0100

    add winning and losing method

--- App/Game.js
+++ App/Game.js
@@ -1,6 +1,9 @@
 import { Quote } from "./Quote.js";
 
 class Game {
+  currentStep = 0;
+  lastStep = 7;
+
   quotes = [
     {
       text: "pan tadeusz",
@@ -33,9 +36,20 @@ class Game {
     this.quote = new Quote(text);
   }
 
-  guess(letter) {
-    this.quote.guess(letter);
-    this.drawQuote();
+  guess(letter, e) {
+    e.target.disabled = true;
+    if (this.quote.guess(letter)) {
+      this.drawQuote();
+    } else {
+      this.currentStep++;
+      document.getElementsByClassName("step")[
+        this.currentStep
+      ].style.opacity = 1;
+
+      if (this.currentStep === this.lastStep) {
+        this.losing();
+      }
+    }
   }
 
   drawLetters() {
@@ -43,17 +57,31 @@ class Game {
       const label = (i + 10).toString(36);
       const button = document.createElement("button");
       button.innerHTML = label;
-      button.addEventListener("click", () => this.guess(label));
+      button.addEventListener("click", (e) => this.guess(label, e));
       this.lettersWrapper.appendChild(button);
     }
+  }
+
+  losing() {
+    this.wordWrapper.innerHTML = "NIESTETY! PRZEGRYWASZ! TO KONIEC GRY!";
+    this.lettersWrapper.innerHTML = "";
+  }
+
+  winning() {
+    this.wordWrapper.innerHTML = "GRATULACJE! WYGYRWASZ! KONIEC GRY!";
+    this.lettersWrapper.innerHTML = "";
   }
 
   drawQuote() {
     const content = this.quote.getContent();
     this.wordWrapper.innerHTML = content;
+    if (!content.includes("_")) {
+      this.winning();
+    }
   }
 
   start() {
+    document.getElementsByClassName("step")[this.currentStep].style.opacity = 1;
     this.drawLetters();
     this.drawQuote();
   }
